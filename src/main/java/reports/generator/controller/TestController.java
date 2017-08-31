d05cbeaf8c8cbada72b0bea202bc4388abfbef13
Himanshu Garg <higarg@expedia.com>
2017-08-31 10:29:41 +0530

    add mysql config

--- src/main/java/reports/generator/controller/TestController.java
+++ src/main/java/reports/generator/controller/TestController.java
@@ -1,19 +1,35 @@
 package reports.generator.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/view")
 public class TestController {
+
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, ModelMap model) {
         model.addAttribute("name", name);
         System.out.println("GRETTINGS, "+ name +"................!");
-        return "greeting";
+        String sql = "SELECT * FROM client_aloc";
+
+        List total = jdbcTemplate.queryForList(sql);
+
+        for(Object l: total){
+            System.out.println(l);
+
+        }
+        return "welcome";
     }
 
 
